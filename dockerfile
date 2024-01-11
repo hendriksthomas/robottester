1cea707d28a85f74d1b8a701c3aff1919cc49f02
Thomas Hendriks <thomas.hendriks@nokia.com>
2024-01-11 18:08:25 +0100

    Cleanup. Add postgres check to container. Make tests independent of robot files passed.

--- dockerfile
+++ dockerfile
@@ -5,13 +5,14 @@ WORKDIR app/
 COPY ./requirements.txt .
 COPY ./hosts /etc/hosts
 COPY entrypoint.sh ./entrypoint.sh
-COPY libraries ./libraries
+COPY *.robot ./robot/
+COPY libraries ./robot/libraries
 
-copy FUNC-BASE-GRPC-02.robot .
+copy push_results_to_postgres.py .
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-RUN apt-get update && apt-get install -y iputils-ping
+RUN apt-get update && apt-get install -y iputils-ping netcat-openbsd
 
 RUN ls -latrh
 RUN pwd
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ echo "Waiting for routers to boot..."
 while ! ping 172.143.177.11 -c 1; do
     sleep 0.1
 done
+echo "Router is up (bof pings)"
 
 #echo "Waiting gRPC to be available..."
 #
@@ -12,9 +13,15 @@ done
 #    sleep 0.1
 #done
 
-echo "Router is up (bof pings)"
+echo "Waiting for postgres..."
+while ! nc -z clab-pce_demo2-postgres 5432; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
 
-robot FUNC-BASE-GRPC-02.robot
+
+robot --name GenericGRPCRobotTests robot/*.robot
 cat output.xml
+python push_results_to_postgres.py
 
 exec "$@"
